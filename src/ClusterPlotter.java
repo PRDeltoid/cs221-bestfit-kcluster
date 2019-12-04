45811f946c03b94302c365f6ba347b5e923bec8d
unknown <tttt@sss.com>
2019-12-03 16:42:30 -0800

    Catchup

--- src/ClusterPlotter.java
+++ src/ClusterPlotter.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -28,18 +29,19 @@ public class ClusterPlotter {
         public double dist(Tuple to) {
             return Math.sqrt(Math.pow(this.x - to.x,2)+Math.pow(this.y - to.y,2));
         }
+
+        public void print() {
+            System.out.print("(" + this.x +", "+ this.y +") ");
+        }
     };
 
     public void plot(String datafile, String outfile, int k, int width, int height) {
-        ImagePlotter plotter = new ImagePlotter();
-        plotter.setWidth(width);
-        plotter.setHeight(height);
 
         // Read data file
         List<Tuple> data = readData(datafile);
 
+        //Generate our initial (random) centers
         Random rand = new Random();
-
         List<Tuple> centers = new ArrayList<>(k);
         for(int i = 0; i<k; i++) {
             centers.add(data.get(rand.nextInt(data.size())));
@@ -47,18 +49,61 @@ public class ClusterPlotter {
 
         System.out.println("Centers:");
         for(Tuple center : centers) {
-            System.out.print("(" + center.x +", "+ center.y +") ");
+            center.print();
+        }
+        System.out.println("\n"); //newline
+
+        //Organize data into clusters based on centers
+        List<List<Tuple>> clusters =  new ArrayList<List<Tuple>>(k);
+        List<Color> clusterColors = new ArrayList<Color>(k);
+        for(int i = 0; i<k; i++) {
+            clusters.add(new ArrayList<Tuple>());
+            clusterColors.add(new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat()));
         }
 
         for(Tuple item : data) {
-            double minDist = 0;
-            for(Tuple center : centers) {
-                if(item.dist(center) < minDist) {
-                    minDist = item.dist(center);
+            double minDist = Double.POSITIVE_INFINITY;
+            int clusterId = -1;
+            //For each item, calculate the distance to all centers
+            //and mark the closest center's clusterId (our "clusters" index)
+            for(int i = 0; i < k; i++) {
+                if(item.dist(centers.get(i)) < minDist) {
+                    //We found a new minimum. Mark it and repeat
+                    minDist = item.dist(centers.get(i));
+                    clusterId = i;
                 }
             }
+            //Add our item to the closest cluster
+            clusters.get(clusterId).add(item);
         }
 
+        //Console output of clusters for debugging
+        for(List<Tuple> cluster : clusters) {
+            System.out.println("[");
+            for(Tuple item : cluster) {
+                item.print();
+            }
+            System.out.println("\n],\nReal Center:");
+            find_center(cluster).print();
+        }
+
+        //Calculate error level
+        /*
+        for(List<Tuple> cluster : clusters) {
+            for (Tuple item : data) {
+
+            }
+        }*/
+
+        //Draw a pretty picture
+        ImagePlotter plotter = new ImagePlotter();
+        plotter.setWidth(width);
+        plotter.setHeight(height);
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < clusters.get(i).size(); j++) {
+                plotter.addPoint((int) clusters.get(i).get(j).x, (int) clusters.get(i).get(j).y); //, clusterColors.get(i));
+            }
+        }
         try {
             plotter.write(outfile);
         } catch (IOException e) {}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         BestFitLinePlotter myBestFitLinePlotter = new BestFitLinePlotter();
-        myBestFitLinePlotter.plot("./data/linedata-1.txt","best_fit_line.png", 400, 400);
+        //myBestFitLinePlotter.plot("./data/linedata-1.txt","best_fit_line.png", 400, 400);
 
         ClusterPlotter myClusterPlotter = new ClusterPlotter();
         myClusterPlotter.plot("./data/clusterdata-2.txt","clusterplot.png",3, 400, 400);
